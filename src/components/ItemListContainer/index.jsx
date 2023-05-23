d44c58dc668ee18ff4c9a8092bf06e244c8a2727
YesiDorado <doradoyesica@gmail.com>
2023-05-23 08:02:56 -0300

    preentrega

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,15 +1,26 @@
 import ItemCount from "../ItemCount";
 import ItemList from "../ItemList";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import title from "../title";
 
-const makeUp =[
-    { id: 1, image: "https://www.pngwing.com/es/free-png-bidbb", title: "Labial"},
-    { id: 2, image: "https://www.pngwing.com/es/free-png-hoacx", title: "Base de Maquillaje"},
-    { id: 3, image: "https://www.freepng.es/png-k84m54/", title: "BSombra de ojos"},
+const makeUp = [
+    { id: 1, image: "https://www.pngwing.com/es/free-png-bidbb", title: "Labial" },
+    { id: 2, image: "https://www.pngwing.com/es/free-png-hoacx", title: "Base de Maquillaje" },
+    { id: 3, image: "https://www.freepng.es/png-k84m54/", title: "BSombra de ojos" },
 ];
 
 export const ItemListContainer = ({ texto }) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const getData = new Promise(resolve => {
+            setTimeout(() => {
+                resolve(makeUp);
+            }, 3000);
+        });
+        getData.then(res => console.log(res));
+
+    }, [])
 
     const onAdd = (quantity) => {
         console.log(`Compraste ${quantity} unidades`);
@@ -19,7 +30,7 @@ export const ItemListContainer = ({ texto }) => {
         <>
             <title greeting={texto} />
             <ItemCount initial={1} stock={5} onAdd={onAdd} />
-            <ItemList />
+            <ItemList data={data} />
         </>
     );
 }
